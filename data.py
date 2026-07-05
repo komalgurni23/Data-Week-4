b021b0916dbb340a754a2ad4137d68fa795eda17
komalgurni23 <komalgurnani2003@gmail.com>
2026-07-05 23:49:50 +0530

    Update data.py

--- data.py
+++ data.py
@@ -2,10 +2,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os
 
-# Create visualization folder
+
 os.makedirs("visualizations", exist_ok=True)
 
-# Load Dataset
+
 try:
     df = pd.read_csv("sales_data.csv")
     print("Dataset Loaded Successfully!\n")
@@ -13,7 +13,7 @@ except FileNotFoundError:
     print("Error: sales_data.csv file not found!")
     exit()
 
-# Display Data
+
 print("First 5 Rows:")
 print(df.head())
 
@@ -26,18 +26,18 @@ print(df.columns)
 print("\nMissing Values:")
 print(df.isnull().sum())
 
-# Data Cleaning
+
 df.drop_duplicates(inplace=True)
 df.dropna(inplace=True)
 
 print("\nDataset after Cleaning:")
 print(df.shape)
 
-# Summary
+
 print("\nSummary Statistics:")
 print(df.describe())
 
-# Analysis
+
 total_sales = df["Total_Sales"].sum()
 print("\nTotal Sales =", total_sales)
 
@@ -50,19 +50,19 @@ print("Highest Sale =", highest_sale)
 lowest_sale = df["Total_Sales"].min()
 print("Lowest Sale =", lowest_sale)
 
-# Product Wise Sales
+
 product_sales = df.groupby("Product")["Total_Sales"].sum()
 
 print("\nProduct Wise Sales")
 print(product_sales)
 
-# Region Wise Sales
+
 region_sales = df.groupby("Region")["Total_Sales"].sum()
 
 print("\nRegion Wise Sales")
 print(region_sales)
 
-# ---------------- Bar Chart ----------------
+
 plt.figure(figsize=(8,5))
 product_sales.plot(kind="bar")
 plt.title("Product Wise Sales")
@@ -72,7 +72,7 @@ plt.tight_layout()
 plt.savefig("visualizations/product_sales.png")
 plt.show()
 
-# ---------------- Pie Chart ----------------
+
 plt.figure(figsize=(7,7))
 region_sales.plot(kind="pie", autopct="%1.1f%%")
 plt.title("Region Wise Sales")
@@ -81,7 +81,7 @@ plt.tight_layout()
 plt.savefig("visualizations/region_sales.png")
 plt.show()
 
-# ---------------- Line Chart ----------------
+
 df["Date"] = pd.to_datetime(df["Date"])
 daily_sales = df.groupby("Date")["Total_Sales"].sum()
 
@@ -95,7 +95,7 @@ plt.tight_layout()
 plt.savefig("visualizations/daily_sales.png")
 plt.show()
 
-# Insights
+
 print("\n========== PROJECT INSIGHTS ==========")
 
 print("Total Sales :", total_sales)
